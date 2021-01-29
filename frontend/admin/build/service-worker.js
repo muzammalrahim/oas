b4e177087b31e363e0c4e1bd55c8a158aebc7602
Rizwan <creative.joomdev@gmail.com>
2021-01-29 15:31:16 +0500

    admin build

--- frontend/admin/build/service-worker.js
+++ frontend/admin/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/metronic/react/demo3/precache-manifest.48eda05e06cf871eeeef2798fb88893a.js"
+  "/metronic/react/demo3/precache-manifest.5521aac01718d01db6d1f73e50a55e97.js"
 );
 
 self.addEventListener('message', (event) => {

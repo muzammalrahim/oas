4a1a92ff00264afbad7ff633cea0bff2a2325b60
Rizwan <creative.joomdev@gmail.com>
2021-01-29 13:00:55 +0500

    removing admin path from backend

--- oas/urls.py
+++ oas/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     url(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^$', schema_view.with_ui('swagger', cache_timeout=0),
